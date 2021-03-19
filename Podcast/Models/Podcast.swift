c766022122eef1b32dbc252aa0d518e91e2c7819
Mika <jmj990411@1163.com>
2021-03-19 15:47:27 +0800

    Update Podcast.swift
    add struct of Podcast and SearchResults

--- Podcast/Models/Podcast.swift
+++ Podcast/Models/Podcast.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Podcast: Decodable {
+    var trackName: String?
+    var artistName: String?
+    var artworkUrl600: String?
+    var trackCount: Int?
+    var feedUrl: String?
+}
+
+struct SearchResults: Decodable {
+    let resultCount: Int
+    let results: [Podcast]
+}
