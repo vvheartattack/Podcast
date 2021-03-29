2aa2b43c933417280247b7fe00fb717a4715ccc9
Mika <jmj990411@1163.com>
2021-03-29 22:04:41 +0800

    Update PodcastDetailViewController.swift

--- Podcast/View/PodcastDetail/PodcastDetailViewController.swift
+++ Podcast/View/PodcastDetail/PodcastDetailViewController.swift
@@ -24,34 +24,80 @@ class PodcastDetailViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let podcastImageView = UIImageView()
-//        podcastImageView.translatesAutoresizingMaskIntoConstraints = false
-//        podcastImageView.contentMode = .scaleAspectFit
-//        self.view.addSubview(podcastImageView)
-//        NSLayoutConstraint.activate([
-//            podcastImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-//            podcastImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-//            podcastImageView.topAnchor.constraint(equalTo: self.view.topAnchor),
-//            podcastImageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
-//        ])
-//        podcastImageView.kf.setImage(with: URL(string: podcast.artworkUrl600!))
-        let episodeImageView = UIImageView()
-        episodeImageView.translatesAutoresizingMaskIntoConstraints = false
-        episodeImageView.contentMode = .scaleAspectFit
-        self.view.addSubview(episodeImageView)
+        
+        let podcastImageView = UIImageView()
+        let podcastTitleLabel = UILabel()
+        let podcastDescriptionLabel = UILabel()
+//        var episodeImageView = UIImageView()
+        let OuterStackView = UIStackView()
+        let podcastStackView = UIStackView()
+//        let podcastImageStackView = UIStackView()
+        let podcastLabelsStackView = UIStackView()
+        let episodeStackView = UIStackView()
+        let detailViewScrollView = UIScrollView()
+
+        // Set up podcastImageView
+        podcastImageView.kf.setImage(with: URL(string: podcast.artworkUrl600!))
+        podcastImageView.layer.cornerRadius = 20
+        podcastImageView.clipsToBounds = true
         NSLayoutConstraint.activate([
-            episodeImageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            episodeImageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            episodeImageView.topAnchor.constraint(equalTo: self.view.topAnchor),
-            episodeImageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+            podcastImageView.widthAnchor.constraint(equalToConstant: 150),
+            podcastImageView.heightAnchor.constraint(equalToConstant: 150),
         ])
-        NetworkManager.shared.fetchEpisodes(feedURL: podcast.feedUrl!, completionHandler: { episodes in
-            self.episodes = episodes
-            DispatchQueue.main.async {
-                print(episodes.map{ $0.imageUrl }.filter{   $0 != nil})
-                episodeImageView.kf.setImage(with: URL(string: episodes[0].imageUrl!))
-            }
-        })
+        
+        
+        // Set up podcastTitleLabel
+        podcastTitleLabel.text = podcast.trackName
+        
+        //Set up podcastDescriptionLabel
+        podcastDescriptionLabel.text = podcast.artistName
+        
+        // Set up OuterStackView
+        OuterStackView.addArrangedSubview(podcastStackView)
+        OuterStackView.addArrangedSubview(episodeStackView)
+        OuterStackView.axis = .vertical
+        
+        // Set up podcastLabelsStackView
+        podcastLabelsStackView.addArrangedSubview(podcastTitleLabel)
+        podcastLabelsStackView.addArrangedSubview(podcastDescriptionLabel)
+        podcastLabelsStackView.axis = .vertical
+        
+
+        
+        
+        // Set up podcastStackView
+        podcastStackView.addArrangedSubview(podcastImageView)
+        podcastStackView.addArrangedSubview(podcastLabelsStackView)
+        podcastStackView.axis = .horizontal
+        podcastStackView.alignment = .top
+        
+        
+        // Set up episodeStackView
+        
+        // Set up detailViewScrollView
+        detailViewScrollView.addSubview(OuterStackView)
+        
+        
+//        OuterStackView.distribution = .fillEqually
+        OuterStackView.translatesAutoresizingMaskIntoConstraints = false
+//        OuterStackView.contentMode = .scaleAspectFit
+        self.view.addSubview(OuterStackView)
+        NSLayoutConstraint.activate([
+            OuterStackView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16),
+            OuterStackView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16),
+            OuterStackView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            OuterStackView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -16)
+        ])
+        
+        
+        
+//        NetworkManager.shared.fetchEpisodes(feedURL: podcast.feedUrl!, completionHandler: { episodes in
+//            self.episodes = episodes
+//            DispatchQueue.main.async {
+//                print(episodes.map{ $0.imageUrl }.filter{   $0 != nil})
+//                detailStackView.kf.setImage(with: URL(string: episodes[0].imageUrl!))
+//            }
+//        })
         
         
         
