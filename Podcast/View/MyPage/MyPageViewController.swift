04877b65b06de51c76d2f74d7eba72f0fe676457
Mika <jmj990411@1163.com>
2021-04-07 23:45:47 +0800

    Update MyPageViewController.swift

--- Podcast/View/MyPage/MyPageViewController.swift
+++ Podcast/View/MyPage/MyPageViewController.swift
@@ -6,12 +6,47 @@
 //
 
 import UIKit
+import SnapKit
 
 class MyPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(true, animated: false)
+        setMyPageLayout()
+        
         // Do any additional setup after loading the view.
     }
+    
+    private func setMyPageLayout() {
+        let avatarImageView: UIImageView
+        
+        // Set avatarImageView
+        avatarImageView = UIImageView()
+        avatarImageView.image = UIImage.init(imageLiteralResourceName: "Avatar.JPG")
+        avatarImageView.clipsToBounds = true
+        avatarImageView.layer.cornerRadius = avatarImageView.frame.size.width / 2
+        avatarImageView.snp.makeConstraints { (make) in
+            make.width.height.equalTo(150)
+        }
+        avatarImageView.contentMode = .scaleAspectFit
+        
+        let outerStackView: UIStackView
+        
+        // Set outerStackView
+        outerStackView = UIStackView()
+        outerStackView.distribution = .fill
+        outerStackView.axis = .vertical
+        outerStackView.alignment = .center
+        self.view.addSubview(outerStackView)
+        outerStackView.snp.makeConstraints { (make) in
+            make.centerX.equalTo(self.view)
+            make.centerY.equalTo(self.view)
+            make.leading.equalTo(self.view)
+            make.trailing.equalTo(self.view)
+        }
+        outerStackView.addArrangedSubview(avatarImageView)
+        
+    }
+    
 }
