9da0bc29c98b28513bdcbb9cda9dcd887efa95a6
Mika <jmj990411@1163.com>
2021-04-09 19:54:21 +0800

    Update ViewController.swift

--- Podcast/ViewController.swift
+++ Podcast/ViewController.swift
@@ -14,10 +14,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        NetworkManager.shared.fetchPodcasts(withSearchKeywords: "nice try") { (podcasts) in
-            self.podcasts = podcasts
-            print(self.podcasts)
-        }
         
         // Do any additional setup after loading the view.
     }
