0d074b6fb28f4fdbd2255e3892c42e71dc86c010
Mika <jmj990411@1163.com>
2021-04-21 12:14:05 +0800

    reoder viewcontrollers of tabview

---
+++ Podcast/View/SubcribedPodcastsView/SubcribedPodcastsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SubcribedPodcastsViewController.swift
+//  Podcast
+//
+//  Created by Mika on 2021/4/21.
+//
+
+import UIKit
+
+class SubcribedPodcastsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
