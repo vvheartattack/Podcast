d8298992ef58bee1b4c0c46cee195b4d572e1838
Mika <jmj990411@1163.com>
2021-05-01 14:22:20 +0800

    Update SubcribedPodcastsViewController.swift

--- Podcast/View/SubcribedPodcastsView/SubcribedPodcastsViewController.swift
+++ Podcast/View/SubcribedPodcastsView/SubcribedPodcastsViewController.swift
@@ -6,24 +6,81 @@
 //
 
 import UIKit
+import GRDB
+import SnapKit
 
 class SubcribedPodcastsViewController: UIViewController {
-
+    var tableView: UITableView!
+    var podcasts: [Podcast] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true) 
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView = UITableView()
+        fetchData()
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        self.navigationController?.hidesBarsOnTap = true
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateSubcribedPodcast(_:)), name: Notification.Name("PoscastSubscriptionUpdate"), object: nil)
+        
+        // Set tableView
+        self.view.addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.top.bottom.trailing.leading.equalToSuperview()
+        }
+        self.tableView.dataSource = self
+        self.tableView.register(SearchViewTableViewCell.self, forCellReuseIdentifier: "datacell")
+        self.tableView.delegate = self
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func didUpdateSubcribedPodcast(_ notification: Notification)
+    {
+        fetchData()
     }
-    */
+    
+    func fetchData() {
+        podcasts = GRDBHelper.shared.fetchAll(Podcast.self)!
+        self.tableView.reloadData()
+    }
+    
+}
 
+extension SubcribedPodcastsViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let podcastDetailVC = PodcastDetailViewController(podcast: podcasts[indexPath.row])
+        self.navigationController?.pushViewController(podcastDetailVC, animated: true)
+    }
+    
+}
+
+extension SubcribedPodcastsViewController: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return podcasts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "datacell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! SearchViewTableViewCell
+        let podcast = podcasts[indexPath.row]
+        cell.cellIamgeView.kf.setImage(with: URL(string: podcast.artworkUrl600!)) { result in
+            cell.setNeedsLayout()
+        }
+        cell.titleLabel.text = podcast.trackName
+        cell.descriptionLabel.text = podcast.artistName
+        cell.titleLabel.sizeToFit()
+        cell.descriptionLabel.sizeToFit()
+        return cell
+    }
+    
+    
+    
 }
