d523bfc87dc89e324d35edc85ef42d0c648a07ab
Mika <jmj990411@1163.com>
2021-05-20 23:24:15 +0800

    Update CommentViewController.swift

--- Podcast/View/CommentView/CommentViewController.swift
+++ Podcast/View/CommentView/CommentViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class CommentViewController: UIViewController {
-    @IBOutlet var inputFieldView: UIView!
+    @IBOutlet var inputFieldView: UITextField!
     @IBOutlet var scrollView: UIScrollView!
-
+    var podcast: Podcast?
+    var episode: Episode?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let inputText = inputFieldView.text!
+        
+        podcast = Podcast(trackId: 1234)
+        episode = Episode(guid: "alsdj", title: "", pubDate: Date(), description: "", author: "", streamUrl: "")
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
@@ -20,6 +26,15 @@ class CommentViewController: UIViewController {
         // Do any additional setup after loading the view.
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShowNotificationHandler), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHideNotificationHandler), name: UIResponder.keyboardWillHideNotification, object: nil)
+        NetworkManager.shared.postComment(withPodcastTrackID: podcast!.trackId, andEpisodeID: episode!.guid, andUserName: GlobalCache.shared.loginResult!.name, andCommentContent: inputText, completionHandler: { result in
+            let comment = Comment(userName: GlobalCache.shared.loginResult!.name, commentContent: inputText, podcastTrackID: self.podcast!.trackId, episodeID: self.episode!.guid)
+            if result.data == comment {
+                print("comment post succeeded")
+            } else {
+                print("comment post failed")
+            }
+            
+        })
     }
     
     @objc func keyboardWillShowNotificationHandler(notification: NSNotification) {
