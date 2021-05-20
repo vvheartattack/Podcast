c786069a57b7d88eb22b42bc44cf8dd105173ac5
JJAYCHEN <31304335+JJAYCHEN1e@users.noreply.github.com>
2021-05-21 00:10:12 +0800

    2021-05-21 00:10:10

--- Podcast/View/CommentView/CommentViewController.swift
+++ Podcast/View/CommentView/CommentViewController.swift
@@ -11,12 +11,17 @@ class CommentViewController: UIViewController {
     @IBOutlet var inputTextField: UITextField!
     @IBOutlet weak var inputFieldView: UIView!
     @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var outerStackView: UIStackView!
+    @IBOutlet var templateStackView: UIStackView!
+    
     var podcast: Podcast?
     var episode: Episode?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let inputText = inputTextField.text!
+        outerStackView.removeArrangedSubview(templateStackView)
         
         podcast = Podcast(trackId: 1234)
         episode = Episode(guid: "alsdj", title: "", pubDate: Date(), description: "", author: "", streamUrl: "")
@@ -33,7 +38,9 @@ class CommentViewController: UIViewController {
         })
         
         NetworkManager.shared.fetchComment(withPodcastTrackID: podcast!.trackId, andEpisodeID: episode!.guid) { result in
-            print(result)
+            result.data.forEach { comment in
+                self.outerStackView.addArrangedSubview(self.generateViewFor(comment: comment))
+            }
         }
     }
     
@@ -58,6 +65,60 @@ class CommentViewController: UIViewController {
     @objc func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
+    }
+    
+    func generateViewFor(comment: Comment) -> UIView {
+        let outerVStack = UIStackView()
+        outerVStack.axis = .vertical
+        outerVStack.distribution = .fill
+        outerVStack.alignment = .fill
+        outerVStack.spacing = 8
+        
+        let innerHStack = UIStackView()
+        innerHStack.axis = .horizontal
+        innerHStack.distribution = .fill
+        innerHStack.alignment = .top
+        innerHStack.spacing = 10
+        outerVStack.addArrangedSubview(innerHStack)
+        
+        let avatar = UIImageView()
+        avatar.layer.masksToBounds = true
+        avatar.layer.cornerRadius = 20
+        avatar.image = UIImage(named: "Avatar")
+        avatar.snp.makeConstraints { make in
+            make.width.height.equalTo(40)
+        }
+        innerHStack.addArrangedSubview(avatar)
+        
+        let innterVStack = UIStackView()
+        innterVStack.axis = .vertical
+        innterVStack.distribution = .fill
+        innterVStack.alignment = .top
+        innterVStack.spacing = 10
+        innerHStack.addArrangedSubview(innterVStack)
+        
+        let nameLabel = UILabel()
+        nameLabel.text = comment.userName
+        nameLabel.textColor = .black
+        nameLabel.font = .systemFont(ofSize: 20, weight: .medium)
+        nameLabel.textAlignment = .left
+        innterVStack.addArrangedSubview(nameLabel)
+        
+        let contentLabel = UILabel()
+        contentLabel.text = comment.commentContent
+        contentLabel.textColor = .black
+        contentLabel.font = .systemFont(ofSize: 17, weight: .regular)
+        contentLabel.textAlignment = .left
+        innterVStack.addArrangedSubview(contentLabel)
+        
+        let timeLabel = UILabel()
+        timeLabel.text = comment.commentContent
+        timeLabel.textColor = .gray
+        timeLabel.font = .systemFont(ofSize: 15, weight: .medium)
+        timeLabel.textAlignment = .right
+        outerVStack.addArrangedSubview(timeLabel)
+        
+        return outerVStack
     }
 
 }
